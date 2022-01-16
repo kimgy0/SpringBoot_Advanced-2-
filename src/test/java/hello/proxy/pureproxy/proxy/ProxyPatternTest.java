8bf1d5546abc0fd338c5b480a1413adeb29b1d7c
01086055223@hanmail.net <01086055223@hanmail.net>
2022-01-17 02:39:29 +0900

    [FIX]프록시 패턴 예제 코드

---
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,45 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import hello.proxy.pureproxy.proxy.code.Subject;
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+
+
+    @Test
+    public void noProxyTest() {
+        //given
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        //when
+        client.execute();
+        client.execute();
+        client.execute();
+        //then
+
+        /*      Result.
+         *   02:26:42.720 [main] INFO hello.proxy.pureproxy.proxy.code.RealSubject - 실제 객체 호출
+         *   02:26:43.735 [main] INFO hello.proxy.pureproxy.proxy.code.RealSubject - 실제 객체 호출
+         *   02:26:44.750 [main] INFO hello.proxy.pureproxy.proxy.code.RealSubject - 실제 객체 호출
+         *
+         *   하지만 이렇게 똑같은 정보를 조회하는데 불필요하게 클라이언트가 서브젝트에 계속 요청을 던지는 것은 불필요하다.
+         *   성능을 더 좋게 하기 위해서는 불변하지 않는 데이터라는 가정하에 해당 처음에 조회했던 데이터를 어딘가에 보관해두고
+         *   두고두고 사용합니다. 이런 것을 캐싱이라고 하는데 이번엔 캐싱으로 적용해보겠다.
+         */
+
+        //캐시가 적용된 테스트 케이스
+    }
+
+    @Test
+    void cacheProxyTest() {
+        Subject realSubject = new RealSubject();
+        Subject cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
